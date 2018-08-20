8ae71dd37307ab4a6d5d5a77baaf8232b7eeb9fa
NerOcrO <nerocro@gmail.com>
2018-08-20 18:34:08 +0200

    add anagram

---
+++ src/anagram.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// https://fr.wikipedia.org/wiki/Anagramme
+
+export const sortBycharCode = wording => wording
+  .toLowerCase()
+  .normalize('NFD')
+  .replace(/[\u0300-\u036f| |']/g, '')
+  .split('')
+  .map(char => char.charCodeAt())
+  .sort((a, b) => a - b)
+  .join('')
+export const isAnagram = (base, anagram) => sortBycharCode(base) === sortBycharCode(anagram)
+
+console.log(isAnagram('abc', 'abdc'))
+console.log(isAnagram('rr', 'rt'))
+console.log(isAnagram('Marion', 'Romain'))
+console.log(isAnagram('çrâne', 'écran'))
+console.log(isAnagram('Le commandant Cousteau', "Tout commença dans l'eau"))
---
+++ test/anagram.js
@@ -0,0 +1,32 @@
+'use strict'
+
+import assert from 'assert'
+import { sortBycharCode, isAnagram } from '../src/anagram'
+
+describe('Sort by char code :', () => {
+  it('Transformation des mots en code de caractères', () => {
+    assert.strictEqual(sortBycharCode('çrâne'), '​​​​​9799101110114​​​​​')
+  })
+})
+
+describe('Anagram :', () => {
+  it('Nombre de lettres différentes', () => {
+    assert.strictEqual(isAnagram('abc', 'abdc'), false)
+  })
+
+  it('Lettres différentes', () => {
+    assert.strictEqual(isAnagram('rr', 'rt'), false)
+  })
+
+  it('Anagramme simple', () => {
+    assert.strictEqual(isAnagram('Marion', 'Romain'), true)
+  })
+
+  it('Anagramme avec caractère spéciaux', () => {
+    assert.strictEqual(isAnagram('çrâne', 'écran'), true)
+  })
+
+  it('Une phrase', () => {
+    assert.strictEqual(isAnagram('Le commandant Cousteau', "Tout commença dans l'eau"), true)
+  })
+})
